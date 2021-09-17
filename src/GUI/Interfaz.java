697713597b0f494998220312109a77b209893df6
Juan Cruz Rossi <juanrossi1969@gmail.com>
2021-09-17 18:56:32 -0300

    Segundo Commit

---
+++ src/GUI/Interfaz.java
@@ -0,0 +1,56 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+
+public class Interfaz extends JFrame {
+
+	private JPanel contentPane;
+	private ImageIcon fondoTetris;
+	private final JLabel lblFondoTetris = new JLabel("");
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interfaz frame = new Interfaz();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Interfaz() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("F:\\Universidad\\TdP\\Proyectos\\Workspace\\TdP_Proyecto2\\src\\images\\icono tetris 2.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 514, 382);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLACK);
+		panel.setBounds(124, 0, 250, 343);
+		contentPane.add(panel);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+	}
+}
---
+++ src/Launcher/Launcher.java
@@ -0,0 +1,10 @@
+package Launcher;
+
+import GUI.*;
+
+public class Launcher {
+	public static void main() {
+		Interfaz presentacion = new Interfaz();
+    	presentacion.setVisible(true);
+	}
+}
