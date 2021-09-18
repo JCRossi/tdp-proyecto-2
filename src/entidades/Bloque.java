2d3feb02a3106137862c995585143b650bd8166a
Ignacio Martín <ignaciomartin159@gmail.com>
2021-09-17 22:32:07 -0300

    Cuarto commit

--- src/entidades/Bloque.java
+++ src/entidades/Bloque.java
@@ -1,5 +1,34 @@
 package entidades;
 
 public class Bloque {
+	private int fila;
+	private int columna;
+	private String color;
+	
+	public Bloque(int fil, int colu, String col) {
+		this.fila = fil;
+		this.columna = colu;
+		this.color = col;
+	}
+	
+	public void setFila(int f) {
+		fila = f;
+	}
+	
+	public void setColumna(int c) {
+		columna = c;
+	}
+	
+	public int getFila() {
+		return fila;
+	}
+	
+	public int getColumna() {
+		return columna;
+	}
+	
+	public String getColor() {
+		return color;
+	}
 
 }
