7f8ab304e52ad29a37596708412ade1f078369b3
unknown <VirgilioocamposUNS@gmail.com>
2021-09-19 19:46:03 -0300

    Incorporado Reloj funcional

--- src/Launcher/Launcher.java
+++ src/Launcher/Launcher.java
@@ -3,7 +3,7 @@ package Launcher;
 import GUI.*;
 
 public class Launcher {
-	public static void main() {
+	public static void main(String [] args) {
 		Interfaz presentacion = new Interfaz();
     	presentacion.setVisible(true);
 	}
--- src/entidades/Reloj.java
+++ src/entidades/Reloj.java
@@ -2,9 +2,18 @@ package entidades;
 
 import Logica.*;
 
-public class Reloj {
+/*
+Debemos hacer algo semejante a esto cuando creamos el reloj. Siendo this una instancia de Logica.
+	
+	this.reloj = new Reloj(this);
+	Thread hiloReloj = new Thread(this.reloj);
+	hiloReloj.start();
+
+*/
+
+public class Reloj implements Runnable {
 	private Logica juego;
-	private float velocidadCaida;
+	private long velocidadCaida;
 	private int minutos;
 	private int segundos;
 	
@@ -12,14 +21,48 @@ public class Reloj {
 		this.juego = juego;
 		minutos = 0;
 		segundos = 0;
-		velocidadCaida = 1.5f;
+		velocidadCaida = 1000;
+		
 	}
+	
+	@Override
+	public void run() {
+		try {
+			while(true) {
+				if(segundos == 59) {
+					avisarPasaUnMinuto();
+				}else
+					segundos++;
+				
+				avisarCaidaTetrimino();
+				Thread.sleep(velocidadCaida);
+			}
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+	}
+
 	
 	private void avisarCaidaTetrimino() {
 		juego.caer();
 	}
 	
 	private void avisarPasaUnMinuto() {
-		
+		segundos = 0;
+		minutos++;
+		velocidadCaida = (long) (velocidadCaida * 0.75);
+	}
+	
+	public int getMinutos(){
+		return minutos;
+	}
+	
+	public int getSegundos() {
+		return segundos;
 	}
 }
+
+	
