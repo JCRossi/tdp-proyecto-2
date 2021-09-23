729c29f42c7dd7f5857a95587dbddac29c1733f1
MatixJ <matijunca@gmail.com>
2021-09-23 19:59:10 -0300

    Update TetriminoI.java

--- src/entidades/TetriminoI.java
+++ src/entidades/TetriminoI.java
@@ -5,40 +5,165 @@ public class TetriminoI extends Tetrimino{
 	public TetriminoI() {
 		blocks = new Bloque[4];
 		for (int i=0; i<4; i++) 
-			blocks[i]= new Bloque(i,4,"celeste");
-		rotation = 0;		
+			blocks[i]= new Bloque(1,6-i,"celeste");
+		rotation = 90;		
 	}
 
 	@Override
 	public void moverDerecha() {
-		// TODO Auto-generated method stub
-		
+		int[][] bloquesConflictivos = null;
+		if(blocks[0].getColumna()!=9) {
+			if (rotation==0) {
+				bloquesConflictivos =  new int[4][2];
+				for (int i=0; i<4; i++) {
+					bloquesConflictivos[i][0] = blocks[i].getFila();
+					bloquesConflictivos[i][1] = blocks[i].getColumna() + 1;
+				}
+			} else {
+				bloquesConflictivos =  new int[1][2];
+				bloquesConflictivos[0][0] = blocks[0].getFila();
+				bloquesConflictivos[0][1] = blocks[0].getColumna() + 1;		
+			}
+			if(!Grilla.chequearColision(bloquesConflictivos)) {
+				for(int i = 0; i<4; i++)
+					blocks[i].setColumna(blocks[i].getColumna()+1);
+			}	
+		}
 	}
 
 	@Override
 	public void moverIzquierda() {
-		// TODO Auto-generated method stub
+		int[][] bloquesConflictivos = null;
+		if(blocks[3].getColumna()!=0) {
+			if (rotation==0) {
+				bloquesConflictivos =  new int[4][2];
+				for (int i=0; i<4; i++) {
+					bloquesConflictivos[i][0] = blocks[i].getFila();
+					bloquesConflictivos[i][1] = blocks[i].getColumna() - 1;
+				}
+			} else {
+				bloquesConflictivos =  new int[1][2];
+				bloquesConflictivos[0][0] = blocks[3].getFila();
+				bloquesConflictivos[0][1] = blocks[3].getColumna() - 1;			
+			}
+			if(!Grilla.chequearColision(bloquesConflictivos)) {
+				for(int i = 0; i<4; i++)
+					blocks[i].setColumna(blocks[i].getColumna()-1);
+			}
+		}
 		
 	}
 
 	@Override
 	public boolean moverAbajo() {
-		return false;
-		
+		boolean colision = false;
+		int[][] bloquesConflictivos = null;
+		if(blocks[3].getFila()!=20) {
+			if (rotation==90) {
+				bloquesConflictivos =  new int[4][2];
+				for (int i=0; i<4; i++) {
+					bloquesConflictivos[i][0] = blocks[i].getFila() + 1;
+					bloquesConflictivos[i][1] = blocks[i].getColumna();
+				}
+			} else {
+				bloquesConflictivos =  new int[1][2];
+				bloquesConflictivos[0][0] = blocks[3].getFila() + 1;
+				bloquesConflictivos[0][1] = blocks[3].getColumna();			
+			}
+			colision = Grilla.chequearColision(bloquesConflictivos);
+			if(!colision) {
+				for(int i = 0; i<4; i++)
+					blocks[i].setFila(blocks[i].getFila()+1);
+			}
+		} else colision = true;
+		return colision;	
 	}
 
 	@Override
 	public void rotarDerecha() {
-		// TODO Auto-generated method stub
-		
+		int[][] bloquesConflictivos = new int[4][2];;
+		if (rotation==0) {
+			int fila = blocks[2].getFila();
+			int columna = blocks[2].getColumna();
+			for(int i = 0; i<4; i++) {
+				bloquesConflictivos[i][0] = fila;
+			}
+			if(columna<1) {
+				bloquesConflictivos[0][1] = 3;
+				bloquesConflictivos[1][1] = 2;
+				bloquesConflictivos[2][1] = 1;
+				bloquesConflictivos[3][1] = 0;
+			} else if(columna>7) {
+				bloquesConflictivos[0][1] = 9;
+				bloquesConflictivos[1][1] = 8;
+				bloquesConflictivos[2][1] = 7;
+				bloquesConflictivos[3][1] = 6;
+			} else {
+				bloquesConflictivos[0][1] = blocks[0].getColumna()+2;
+				bloquesConflictivos[1][1] = blocks[1].getColumna()+1;
+				bloquesConflictivos[2][1] = columna;
+				bloquesConflictivos[3][1] = blocks[3].getColumna()-1;
+			}
+			if(!Grilla.chequearColision(bloquesConflictivos)) {
+				for(int i = 0; i<4; i++) {
+					blocks[i].setFila(bloquesConflictivos[i][0]);
+					blocks[i].setColumna(bloquesConflictivos[i][1]);	
+				}
+				rotation=90;
+
+			}
+		} else {
+			int fila = blocks[2].getFila();
+			int columna = blocks[2].getColumna();
+			for(int i = 0; i<4; i++) {
+				bloquesConflictivos[i][1] = columna;
+			}
+			if(fila<2) {
+				bloquesConflictivos[0][0] = 0;
+				bloquesConflictivos[1][0] = 1;
+				bloquesConflictivos[2][0] = 2;
+				bloquesConflictivos[3][0] = 3;
+			} else if(fila>19) {
+				bloquesConflictivos[0][0] = 17;
+				bloquesConflictivos[1][0] = 18;
+				bloquesConflictivos[2][0] = 19;
+				bloquesConflictivos[3][0] = 20;
+			} else {
+				bloquesConflictivos[0][0] = blocks[0].getFila()-2;
+				bloquesConflictivos[1][0] = blocks[1].getFila()-1;
+				bloquesConflictivos[2][0] = fila;
+				bloquesConflictivos[3][0] = blocks[3].getFila()+1;
+			}
+			if(!Grilla.chequearColision(bloquesConflictivos)) {
+				for(int i = 0; i<4; i++) {
+					blocks[i].setFila(bloquesConflictivos[i][0]);
+					blocks[i].setColumna(bloquesConflictivos[i][1]);	
+				}
+				rotation=0;
+
+			}
+		}
 	}
 
 	@Override
 	public void rotarIzquierda() {
-		// TODO Auto-generated method stub
-		
+		rotarDerecha();
 	}
 	
-	
+	public int[] filasOcupadas() {
+		int[] filasOcupadas;
+		if(rotation==0) {
+			filasOcupadas = new int[4];
+			
+			filasOcupadas[0] = blocks[0].getFila();
+			filasOcupadas[1] = blocks[1].getFila();
+			filasOcupadas[2] = blocks[2].getFila();
+			filasOcupadas[3] = blocks[3].getFila();
+		} else {
+			filasOcupadas = new int[1];			
+			filasOcupadas[0] = blocks[2].getFila();
+		}
+		return filasOcupadas;
+	}
 
 }
